3ac3dcdf1ab025452be862a319d69beb24fa2f44
Kelyton Santos <kelytonlucas@gmail.com>
2022-09-20 20:07:57 -0300

    funcionamento por módulos

--- course/src/main/java/academy/devdojo/youtube/course/CourseApplication.java
+++ course/src/main/java/academy/devdojo/youtube/course/CourseApplication.java
@@ -2,8 +2,12 @@ package academy.devdojo.youtube.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan({"academy.devdojo.youtube.core.model"})//busca no academy(é um bean de onde encontrar)
+@EnableJpaRepositories({"academy.devdojo.youtube.core.repository"})
 public class CourseApplication {
 
     public static void main(String[] args) {
